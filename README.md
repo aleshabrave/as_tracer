ac6ad056740636f301f76da6d9656e31e784641d
Alexey Smelik <aleshabrave@gmail.com>
2022-06-13 14:20:36 +0500

    Update README.md and add retries

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
-# tracer
-Трассировка автономных систем.
-Traceroute на минималках.
-
-### Usage:
-```
-python3 tracer.py google.com
-```
-
-![img.png](img.png)
-
-
-#### Полное описание можно посмотреть так -
-```
-python3 tracer.py --help
-```
-
-# Подготовка к запуску
-Возможно, перед запуском надо написать -
+# as_tracer
+Simple traceroute util.
+The utility finds the path to the specified hostname and displays
+IP, autonomous system (AS), ISP and country routers in a pretty table.
+There are some parameters: `port` for listing traceroute socket and to send messages,
+`timeout` for receiving datagrams from route nodes, `retries` to ask route nodes and `maximum nodes` in route.
+ 
+### The requirements must be set before use:
+* python ~=3.8
 ```
 python3 -m pip install -r requirements.txt
 ```
+
+**_You can run the utility with the -h flag to see how the parameters are passed:_**
+```
+python3 tracer.py -h
+```
+
+### Usage example:
+```
+python3 tracer.py yander.ru
+```
+
+![img_1.png](img_1.png)
--- tracer.py
+++ tracer.py
@@ -3,13 +3,14 @@ import asyncio
 import socket
 from collections import namedtuple
 from dataclasses import dataclass
-from typing import Iterator, List
+from typing import Iterator, List, Tuple
 
 import aiohttp
 from prettytable import PrettyTable
 
-Settings = namedtuple("Settings", ["port", "timeout", "nodes", "ip"])
-URL_TEMPLATE = f"https://ipinfo.io/{0}/json".format
+Settings = namedtuple("Settings", ["port", "timeout", "nodes", "ip", "retries"])
+AskNodeResult = namedtuple("AskNodeResult", ["need_retry", "needed_ip", "node"])
+URL_TEMPLATE = "https://ipinfo.io/{0}/json".format
 
 
 @dataclass
@@ -25,7 +26,6 @@ async def get_more_info(ip: str) -> List[str]:
         async with session.get(URL_TEMPLATE(ip)) as response:
             result = ["-", "-"]
             response_json = await response.json()
-
             if "org" in response_json:
                 result[0] = response_json["org"]
             if "country" in response_json:
@@ -38,36 +38,56 @@ async def get_route(settings: Settings) -> Iterator[RouteNode]:
     route = []
 
     with socket.socket(
-            socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP
+        socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP
     ) as receiver:
         receiver.bind(("", settings.port))
 
         for node_id in range(settings.nodes):
             with socket.socket(
-                    socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+                socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
             ) as sender:
                 ttl = node_id + 1
                 sender.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
-                sender.sendto(b"hello, world!", (settings.ip, settings.port))
 
-            try:
-                receiver.settimeout(settings.timeout)
-                _, address = receiver.recvfrom(2 ** 16 - 1)
-                receiver.settimeout(None)
-                ip, _ = address
+                for retry in range(settings.retries):
+                    ask_result = await ask_route_node(settings, node_id, sender, receiver)
+                    if not ask_result.need_retry:
+                        break
 
-                as_name, country = await get_more_info(ip)
-                route.append(RouteNode(id=node_id, ip=ip, as_name=as_name, country=country))
-            except socket.error:
-                route.append(RouteNode(id=node_id))
+                route.append(ask_result.node)
+
+                if ask_result.needed_ip:
+                    break
 
     return route
 
 
-def print_route(route: Iterator[RouteNode]) -> None:
-    print("Building a routing table...")
-    table = PrettyTable(["Номер", "IP", "AS", "Страна"])
+async def ask_route_node(
+    settings: Settings, node_id: int, sender: socket.socket, receiver: socket.socket
+) -> AskNodeResult:
+    sender.sendto(b"hello, world!", (settings.ip, settings.port))
 
+    try:
+        receiver.settimeout(settings.timeout)
+        _, address = receiver.recvfrom(2**16 - 1)
+        receiver.settimeout(None)
+        ip, _ = address
+
+        as_name, country = await get_more_info(ip)
+
+        return AskNodeResult(
+            need_retry=False,
+            needed_ip=ip == settings.ip,
+            node=RouteNode(id=node_id, ip=ip, as_name=as_name, country=country),
+        )
+    except socket.timeout:
+        return AskNodeResult(
+            need_retry=True, needed_ip=False, node=RouteNode(id=node_id)
+        )
+
+
+def print_route(route: Iterator[RouteNode]) -> None:
+    table = PrettyTable(["Номер", "IP", "AS", "Страна"])
     for node in route:
         table.add_row([node.id, node.ip, node.as_name, node.country])
     print(f"Routing table:\n{table}")
@@ -98,6 +118,13 @@ def get_settings() -> Settings:
         help="Timeout for receiving datagrams from route nodes.",
     )
     parser.add_argument(
+        "-r",
+        dest="retires",
+        default=1,
+        type=int,
+        help="Retries to ask route nodes.",
+    )
+    parser.add_argument(
         "-n", dest="nodes", default=25, type=int, help="Maximum nodes in route."
     )
     parser.add_argument("hostname", type=str, help="Hostname or ip-address.")
@@ -106,11 +133,12 @@ def get_settings() -> Settings:
 
     ip_address = get_ip(args.hostname)
 
-    return Settings(args.port, args.timeout, args.nodes, ip_address)
+    return Settings(args.port, args.timeout, args.nodes, ip_address, args.retires)
 
 
 async def main():
     route_settings = get_settings()
+    print("Building a routing table...")
     route_iter = await get_route(route_settings)
     print_route(route_iter)
 
