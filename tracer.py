440cc82e3bf27ddb7882dee8aefa0dc79da181cc
Loverboy <diadool@list.ru>
2022-06-13 12:45:27 +0500

    +async

--- tracer.py
+++ tracer.py
@@ -1,14 +1,15 @@
 import argparse
-import json
+import asyncio
 import socket
 from collections import namedtuple
 from dataclasses import dataclass
-from typing import Iterator, Tuple
+from typing import Iterator, List
 
-import requests
+import aiohttp
 from prettytable import PrettyTable
 
 Settings = namedtuple("Settings", ["port", "timeout", "nodes", "ip"])
+URL_TEMPLATE = f"https://ipinfo.io/{0}/json".format
 
 
 @dataclass
@@ -19,37 +20,48 @@ class RouteNode:
     country: str = "-"
 
 
-def get_more_info(ip: str) -> Tuple[str, str]:
-    try:
-        response = requests.get(f"https://ipinfo.io/{ip}/json")
-        response_json = json.loads(response.content)
-        return response_json["org"], response_json["country"]
-    except Exception:
-        return "-", "-"
+async def get_more_info(ip: str) -> List[str]:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(URL_TEMPLATE(ip)) as response:
+            result = ["-", "-"]
+            response_json = await response.json()
+
+            if "org" in response_json:
+                result[0] = response_json["org"]
+            if "country" in response_json:
+                result[1] = response_json["country"]
+
+            return result
 
 
-def get_route(settings: Settings) -> Iterator[RouteNode]:
-    for node_id in range(settings.nodes):
-        ttl = node_id + 1
+async def get_route(settings: Settings) -> Iterator[RouteNode]:
+    route = []
 
-        with socket.socket(
+    with socket.socket(
             socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP
-        ) as receiver:
-            receiver.bind(("", settings.port))
-            receiver.settimeout(settings.timeout)
+    ) as receiver:
+        receiver.bind(("", settings.port))
 
+        for node_id in range(settings.nodes):
             with socket.socket(
-                socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+                    socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
             ) as sender:
+                ttl = node_id + 1
                 sender.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
-                sender.sendto(b"_", (settings.ip, settings.port))
+                sender.sendto(b"hello, world!", (settings.ip, settings.port))
 
             try:
-                _, address = receiver.recvfrom(2**16 - 1)
-                as_name, country = get_more_info(address[0])
-                yield RouteNode(id=ttl, ip=address[0], as_name=as_name, country=country)
+                receiver.settimeout(settings.timeout)
+                _, address = receiver.recvfrom(2 ** 16 - 1)
+                receiver.settimeout(None)
+                ip, _ = address
+
+                as_name, country = await get_more_info(ip)
+                route.append(RouteNode(id=node_id, ip=ip, as_name=as_name, country=country))
             except socket.error:
-                yield RouteNode(id=node_id)
+                route.append(RouteNode(id=node_id))
+
+    return route
 
 
 def print_route(route: Iterator[RouteNode]) -> None:
@@ -58,8 +70,7 @@ def print_route(route: Iterator[RouteNode]) -> None:
 
     for node in route:
         table.add_row([node.id, node.ip, node.as_name, node.country])
-    print("Routing table:")
-    print(table)
+    print(f"Routing table:\n{table}")
 
 
 def get_ip(hostname: str) -> str:
@@ -77,7 +88,7 @@ def get_settings() -> Settings:
         dest="port",
         default=33434,
         type=int,
-        help="Port for listing traceroute socket.",
+        help="Port for listing traceroute socket and to send messages.",
     )
     parser.add_argument(
         "-t",
@@ -87,7 +98,7 @@ def get_settings() -> Settings:
         help="Timeout for receiving datagrams from route nodes.",
     )
     parser.add_argument(
-        "-n", dest="nodes", default=30, type=int, help="Maximum nodes in route."
+        "-n", dest="nodes", default=25, type=int, help="Maximum nodes in route."
     )
     parser.add_argument("hostname", type=str, help="Hostname or ip-address.")
 
@@ -98,7 +109,12 @@ def get_settings() -> Settings:
     return Settings(args.port, args.timeout, args.nodes, ip_address)
 
 
-if __name__ == "__main__":
+async def main():
     route_settings = get_settings()
-    route_iter = get_route(route_settings)
+    route_iter = await get_route(route_settings)
     print_route(route_iter)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
